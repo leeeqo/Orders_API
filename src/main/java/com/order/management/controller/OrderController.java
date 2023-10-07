d2f01b01a42744406dd460ba138fb05415f419c5
leeeqo <105232468+leeeqo@users.noreply.github.com>
2023-10-07 22:56:23 +0300

    Version 1.0: Added: 1. OrderCreation - to simplify adding Orders 2. OrderLineController was commented to prevent operations with OrderLines 3. OrderSummary - for getting the most important info about Orders

--- src/main/java/com/order/management/controller/OrderController.java
+++ src/main/java/com/order/management/controller/OrderController.java
@@ -6,6 +6,7 @@ package com.order.management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import com.order.management.service.ProductService;
 import com.order.management.service.OrderService;
 import com.order.management.service.CustomerService;
@@ -14,10 +15,14 @@ import com.order.management.entity.Order;
 import com.order.management.entity.Product;
 import com.order.management.entity.Customer;
 import com.order.management.entity.OrderLine;
-import java.util.Optional;
+
 import java.util.List;
 
-import org.springframework.boot.context.config.ConfigDataNotFoundException;
+import com.order.management.utils.MappingUtils;
+import java.util.stream.Collectors;
+import com.order.management.dto.OrderSummary;
+import com.order.management.dto.OrderCreation;
+import java.util.Arrays;
 
 /**
  *
@@ -40,22 +45,55 @@ public class OrderController {
     @Autowired
     private OrderLineService orderLineService;
     
-    @GetMapping("/")
+    @Autowired
+    private MappingUtils mappingUtils;
+    
+    /*@GetMapping("/")
     public List<Order> showAllOrders() {
         return orderService.getAllOrders();
+    }*/
+    
+    @GetMapping
+    public List<OrderSummary> showAllOrders() {
+        return orderService.getAllOrders().stream()
+                .map(mappingUtils::orderToSummary)
+                .collect(Collectors.toList());
     }
     
-    @GetMapping("/{id}")
+    /*@GetMapping("/{id}")
     public Order getOrder(@PathVariable int id) {
         Order order = orderService.getOrder(id);
         
         return order;
+    }*/
+    
+    @GetMapping("/{id}")
+    public OrderSummary getOrder(@PathVariable int id) {
+        Order order = orderService.getOrder(id);
+        
+        return mappingUtils.orderToSummary(order);
     }
     
-    @PostMapping("/{customer_id}")
+    @GetMapping("/product/{product_id}")
+    public List<Order> getOrdersByProduct(@PathVariable int product_id) {
+        Product product = productService.getProduct(product_id);
+        List<Order> orders = orderService.findAllByProduct(product);
+        
+        return orders;
+    }
+    
+    @GetMapping("/customer/{customer_id}")
+    public List<Order> getOrdersByCustomer(@PathVariable int customer_id) {
+        Customer customer = customerService.getCustomer(customer_id);
+        List<Order> orders = orderService.findAllByCustomer(customer);
+        
+        return orders;
+    }
+    
+    /*@PostMapping("/{customer_id}")
     public Order addNewOrder (@PathVariable int customer_id
             , @RequestBody Order order) {
-        Order orderToSave = new Order(order.getNumOrderLines()
+        Order orderToSave = new Order(0
                 , order.getSubmissionDate());
         Customer customerToOrder = customerService.getCustomer(customer_id);
         if (customerToOrder != null)
@@ -64,14 +102,48 @@ public class OrderController {
         orderService.saveOrder(orderToSave);
         
         return order;
+    }*/
+    
+    @PostMapping
+    public Order addNewOrder(@RequestBody OrderCreation orderCreation) {
+        Order order = mappingUtils.orderCreationToOrder(orderCreation);
+        orderService.saveOrder(order);
+        
+        List<OrderLine> orderLines = mappingUtils.orderCreationToOrderLines(orderCreation);
+        for (OrderLine orderLine : orderLines) {
+            orderLine.setOrder(order);
+            orderLineService.saveOrderLine(orderLine);
+        }
+        
+        return order;
     }
     
-    @PutMapping
+    @PostMapping("/all")
+    public Order[] addNewOrder(@RequestBody OrderCreation[] orderCreation) {
+        
+        Order[] orders = Arrays.stream(orderCreation)
+                .map(mappingUtils::orderCreationToOrder)
+                .toArray(Order[]::new);
+        
+        for (int i = 0; i < orderCreation.length; i++) {
+            orderService.saveOrder(orders[i]);
+            
+            List<OrderLine> orderLines = mappingUtils.orderCreationToOrderLines(orderCreation[i]);
+            for (OrderLine orderLine : orderLines) {
+                orderLine.setOrder(orders[i]);
+                orderLineService.saveOrderLine(orderLine);
+            }
+        }
+        
+        return orders;
+    }
+    
+    /*@PutMapping
     public Order updateOrder(@RequestBody Order order) {
         orderService.saveOrder(order);
         
         return order;
-    }
+    }*/
     
     @PutMapping("/{id}/{product_id}/{num_products}")
     public Order updateNumProductsInOrder(@PathVariable int id
@@ -96,21 +168,5 @@ public class OrderController {
         orderService.deleteOrder(id);
         
         return "Order with ID = " + id + " was deleted.";
-    }
-    
-    @GetMapping("/product/{product_id}")
-    public List<Order> getOrdersByProduct(@PathVariable int product_id) {
-        Product product = productService.getProduct(product_id);
-        List<Order> orders = orderService.findAllByProduct(product);
-        
-        return orders;
-    }
-    
-    @GetMapping("/customer/{customer_id}")
-    public List<Order> getOrdersByCustomer(@PathVariable int customer_id) {
-        Customer customer = customerService.getCustomer(customer_id);
-        List<Order> orders = orderService.findAllByCustomer(customer);
-        
-        return orders;
     }
 }
